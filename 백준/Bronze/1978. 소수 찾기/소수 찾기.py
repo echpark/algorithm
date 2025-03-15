60d989bf3e2d1d3bf65bb04880ccc4a430f888c0
echpark <ghkqh09@gmail.com>
2025-03-15 14:51:05 +0900

    [Bronze II] Title: 소수 찾기, Time: 36 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -0,0 +1,19 @@
+import math
+
+N = int(input())
+mylist = list(map(int, input().split()))
+
+def isPrime(number):
+    if number < 2:
+        return 0
+    else:
+        for i in range(2, number):
+            if number % i == 0:
+                return 0
+        return 1
+
+count = 0
+for number in mylist:
+    count += isPrime(number)
+
+print(count)
