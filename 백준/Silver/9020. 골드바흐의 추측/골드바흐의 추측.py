dff76f3a032886fb81f83c6812f60975019fec0b
echpark <ghkqh09@gmail.com>
2025-03-17 14:00:19 +0900

    [Silver II] Title: 골드바흐의 추측, Time: 1988 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
@@ -3,18 +3,16 @@ T = int(input())
 def isPrime(num):
     if num < 2:
         return False
-    else:
-        for i in range(2, int(num ** 1/2)+1):
-            if num % i == 0:
-                return False
-        return True
 
-for _ in range(T):
-    m = int(input())
-    n = m // 2
+    for i in range(2, int(num ** 1/2) + 1):
+        if num % i == 0:
+            return False
+    return True
 
-    for x in range(n, 1, -1):
-        y = m - x
-        if isPrime(x) and isPrime(y):
-            print(x, y)
-            break;
+for i in range(T):
+    M = int(input())
+    N = M//2
+    for j in range(N, 1, -1):
+        if isPrime(j) and isPrime(M-j):
+            print(j, M-j)
+            break
