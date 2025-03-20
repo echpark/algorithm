226737f79a0ade9cc058c59a9aa3c00177988488
echpark <ghkqh09@gmail.com>
2025-03-20 10:20:35 +0900

    [Bronze I] Title: 더하기 사이클, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
@@ -1,23 +1,23 @@
-N = input() # N = '1'
+# 2025-03-20 시험
 
-if len(N) == 1:
-    N = '0' + N
-
-cnt = 0
-
-def letscnt(num): # num = '1'
+def findcycle(num):
     global cnt
 
-    if num == N and cnt > 0:
+    # 종료조건 : 처음 주어진 수와 재귀로 들어온 수가 같으면 종료 (첫 호출 제외)
+    if cnt > 0 and int(N) == int(num):
         return
 
-    mysum = str(int(num[0]) + int(num[1])) # mysum = '1'
-    if len(mysum) == 1:
-        mysum = '0' + mysum # mysum = '01'
-    newNum = num[1] + mysum[1] # newNum = '11'
-    cnt += 1
+    # 들어온 수의 자리수를 모두 더한 수
+    plusnum = 0
+    for i in range(len(num)): # 자리수만큼 돌리면서 합을 구함
+        plusnum += int(num[i])
 
-    letscnt(newNum)
+    newnum = str(int(num) % 10) + str(plusnum % 10) # 문자열로 만들어 단순 합침
+    cnt += 1 # 사이클이 한 번 도므로 cnt += 1
+    findcycle(newnum) # 종료조건을 만족하지 못했으므로 재귀 호출
 
-letscnt(N)
+N = input() # 문자열로 수를 받음
+cnt = 0
+
+findcycle(N)
 print(cnt)
