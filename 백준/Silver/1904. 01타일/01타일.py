03f5943b23578f2b4b2c70338377de6c111226e2
echpark <ghkqh09@gmail.com>
2025-04-03 14:20:37 +0900

    [Silver III] Title: 01타일, Time: 96 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.py
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -4,14 +4,9 @@ if N == 1:
     print(1)
 
 else:
-    dp = [0] * (N+1)
-
-    num = 15746
-
-    dp[1] = 1
-    dp[2] = 2
+    a, b = 1, 2
 
     for i in range(3, N+1):
-        dp[i] = ((dp[i-1] % num) + (dp[i-2] % num)) % num
+        a, b = b, (a+b) % 15746
 
-    print(dp[N] % num)
+    print(b)
