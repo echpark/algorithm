834d8e5dea2c8ed95305752f24f37a9b535580fa
echpark <ghkqh09@gmail.com>
2025-09-15 13:31:55 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 9.14 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -0,0 +1,16 @@
+from itertools import combinations as c
+
+def solution(clothes):
+    clothes_dict = dict()
+    
+    for cloth in clothes:
+        if cloth[1] not in clothes_dict:
+            clothes_dict[cloth[1]] = 1
+        else:
+            clothes_dict[cloth[1]] += 1
+    
+    answer = 1
+    for cnt in clothes_dict.values():
+        answer *= (cnt + 1)
+    
+    return answer - 1
